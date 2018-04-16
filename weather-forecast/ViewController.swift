f585b8575c1a6f61b2c73f01477f632c1aed96e9
xiongxhc <xionghc713@gmail.com>
2018-04-17 04:40:06 +0800

    UIsetup

---
+++ weather-forecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather-forecast
+//
+//  Created by Xiong Hang Chen on 17/4/18.
+//  Copyright © 2018 Xiong Hang Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
